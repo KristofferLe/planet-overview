cccc2e0ca632b48233981c7d692ff78983dacb0a
Kristoffer <kristoffer.lettegard@outlook.com>
2024-07-04 19:31:50 +0100

    changed the with for the wrapper

--- src/assets/styled-components/background-wrapper/backgroundWrapper.jsx
+++ src/assets/styled-components/background-wrapper/backgroundWrapper.jsx
@@ -3,6 +3,7 @@ import background from "../../images/planet-background.png";
 
 export const Wrapper = styled.div`
   height: 100%;
+  width: 100%;
   background-image: url(${background});
   background-size: cover;
   background-position: center;
